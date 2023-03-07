5af863ee83accefa5c553b3e8971c42f5915f41e
AleksandrTikka <alexandertikka@gmail.com>
2023-03-08 00:26:20 +0200

    tabBar styled

--- screens/main/Home.jsx
+++ screens/main/Home.jsx
@@ -8,7 +8,7 @@ import { SimpleLineIcons } from "@expo/vector-icons";
 // import { AntDesign } from "@expo/vector-icons";
 import { Feather } from "@expo/vector-icons";
 
-import { View, Text, StyleSheet } from "react-native";
+import { View, TouchableOpacity, StyleSheet } from "react-native";
 const MainTab = createBottomTabNavigator();
 export default function Home() {
   return (
@@ -16,23 +16,64 @@ export default function Home() {
       initialRouteName="Posts"
       screenOptions={{
         tabBarShowLabel: false,
+        tabBarStyle: {
+          paddingHorizontal: 70,
+          paddingTop: 9,
+          paddingBottom: 34,
+          height: 83,
+          borderTopWidth: 1,
+          borderTopColor: "rgba(0, 0, 0, 0.3)",
+        },
+
         headerTitleAlign: "center",
         headerTitleStyle: {
           fontFamily: "Roboto-Medium",
           fontSize: 17,
           color: "##212121",
+          lineHeight: 22,
         },
-        headerStyle: { height: 88, marginBottom: 11 },
+        headerStyle: {
+          //   height: 88,
+
+          borderBottomWidth: 1,
+          borderBottomColor: "rgba(0, 0, 0, 0.3)",
+        },
       }}
     >
       <MainTab.Screen
         options={{
           //   headerShown: false,
           tabBarIcon: ({ focused, size, color }) => (
-            <SimpleLineIcons name="grid" size={size} color={color} />
+            <View
+              style={{
+                ...styles.iconWrapper,
+                backgroundColor: focused ? "#FF6C00" : "#FFFFFF",
+              }}
+            >
+              <SimpleLineIcons
+                name="grid"
+                size={24}
+                color={focused ? "#FFFFFF" : "#212121"}
+              />
+            </View>
           ),
 
           title: "Публикации",
+          //   headerStyle: {
+          //     height: 88,
+          //   },
+          headerRight: () => (
+            <TouchableOpacity style={{ paddingRight: 19 }}>
+              <Feather
+                name="log-out"
+                size={24}
+                color="#BDBDBD"
+                onPress={() => {
+                  alert("нажата кнопка Logout");
+                }}
+              />
+            </TouchableOpacity>
+          ),
         }}
         name="Posts"
         component={PostsScreen}
@@ -41,14 +82,38 @@ export default function Home() {
         options={{
           //   headerShown: false,
           tabBarIcon: ({ focused, size, color }) => (
-            <View style={styles.plus}>
-              <Feather name="plus" size={20} color="#FFFFFF" />
+            <View
+              style={{
+                ...styles.iconWrapper,
+                backgroundColor: focused ? "#FF6C00" : "#FFFFFF",
+              }}
+            >
+              <Feather
+                name="plus"
+                size={24}
+                color={focused ? "#FFFFFF" : "#212121"}
+              />
             </View>
           ),
           title: "Создать публикацию",
-          headerBackButtonMenuEnabled: true,
-          headerBackVisible: true,
-          headerBackTitleVisible: true,
+          //   headerStyle: {
+          //     height: 88,
+          //   },
+          //   headerBackButtonMenuEnabled: true,
+          //   headerBackVisible: true,
+          //   headerBackTitleVisible: true,
+          headerLeft: () => (
+            <TouchableOpacity style={{ paddingLeft: 20 }}>
+              <Feather
+                name="arrow-left"
+                size={24}
+                color="#212121CC"
+                onPress={() => {
+                  alert("нажата кнопка назад");
+                }}
+              />
+            </TouchableOpacity>
+          ),
         }}
         name="Create"
         component={CreatePostsScreen}
@@ -57,7 +122,18 @@ export default function Home() {
         options={{
           headerShown: false,
           tabBarIcon: ({ focused, size, color }) => (
-            <Feather name="user" size={size} color={color} />
+            <View
+              style={{
+                ...styles.iconWrapper,
+                backgroundColor: focused ? "#FF6C00" : "#FFFFFF",
+              }}
+            >
+              <Feather
+                name="user"
+                size={24}
+                color={focused ? "#FFFFFF" : "#212121"}
+              />
+            </View>
           ),
         }}
         name="Profile"
@@ -67,12 +143,12 @@ export default function Home() {
   );
 }
 const styles = StyleSheet.create({
-  plus: {
+  iconWrapper: {
     justifyContent: "center",
     alignItems: "center",
     width: 70,
     height: 40,
-    backgroundColor: "#FF6C00",
+    // backgroundColor: "#FF6C00",
     borderRadius: 20,
   },
 });
